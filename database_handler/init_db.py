c78c0d9b7a3fa1a4c57c09463c2898c79d716215
ben <bobusbogdan@gmail.com>
2023-02-02 17:44:41 +0100

    pobieranie listy polaczen bez dat jeszcze

--- database_handler/init_db.py
+++ database_handler/init_db.py
@@ -26,6 +26,35 @@ class Database:
     def close_connection(self):
         self._connection.close()
 
+    def list_cities(self):
+        list_city_query = """
+        SELECT * FROM cities
+        """
+        city_list = []
+        temp = []
+        with self._connection.cursor() as cursor:
+            cursor.execute(list_city_query)
+            temp.append(cursor.fetchall())
+
+        for city in temp:
+            for field in city:
+                city_list.append(field[0])
+        return city_list
+
+    def list_connections(self, source, destination):
+        list_connections_query = """
+        SELECT * FROM rides WHERE 
+        src = %s
+        AND destination = %s
+        """
+
+
+        connection_values = (source, destination)
+        with self._connection.cursor() as cursor:
+            cursor.execute(list_connections_query, connection_values)
+            return cursor.fetchall()
+
+
     def create_users_table(self):
         create_users_table_query = """
            CREATE TABLE IF NOT EXISTS users (
@@ -44,15 +73,15 @@ class Database:
     def create_cities_table(self):
         create_cities_table_query = """
         CREATE TABLE IF NOT EXISTS cities (
-        city varchar(255) NOT NULL UNIQUE)"""
+            city varchar(255) NOT NULL UNIQUE)"""
 
         insert_into_cities_query = """
         INSERT IGNORE INTO cities (city) VALUES
-        ('Toruń'),
-        ('Kraków'),
-        ('Warszawa'),
-        ('Bydgoszcz'),
-        ('Rzeszów')
+            ('Toruń'),
+            ('Kraków'),
+            ('Warszawa'),
+            ('Bydgoszcz'),
+            ('Rzeszów')
         """
 
         with self._connection.cursor() as cursor:
@@ -63,15 +92,15 @@ class Database:
     def create_ticket_table(self):
         create_ticket_table_query = """
         CREATE TABLE IF NOT EXISTS ticket (
-        ticket_id VARCHAR(20) NOT NULL PRIMARY KEY,
-        ride_id VARCHAR(20) NOT NULL,
-        user_id int(11) NOT NULL,
-        seat_no int(11) NOT NULL,
-        cost int(11) NOT NULL,
-        FOREIGN KEY (ride_id)
-            REFERENCES rides(ride_id),
-        FOREIGN KEY (user_id)
-            REFERENCES users(user_id)
+            ticket_id VARCHAR(20) NOT NULL PRIMARY KEY,
+            ride_id VARCHAR(20) NOT NULL,
+            user_id int(11) NOT NULL,
+            seat_no int(11) NOT NULL,
+            cost int(11) NOT NULL,
+            FOREIGN KEY (ride_id)
+                REFERENCES rides(ride_id),
+            FOREIGN KEY (user_id)
+                REFERENCES users(user_id)
         ) """
 
         delete_check_seat_trigger_query = """
@@ -80,9 +109,9 @@ class Database:
         check_seat_no_trigger = """
         CREATE TRIGGER check_seat_no BEFORE INSERT ON ticket
         FOR EACH ROW
-        IF NEW.seat_no > (SELECT seats FROM rides r WHERE r.ride_id = NEW.ride_id)
+            IF NEW.seat_no > (SELECT seats FROM rides r WHERE r.ride_id = NEW.ride_id)
         THEN 
-        SIGNAL SQLSTATE '50002' SET MESSAGE_TEXT = 'Niepoprawny numer miejsca';
+            SIGNAL SQLSTATE '50002' SET MESSAGE_TEXT = 'Niepoprawny numer miejsca';
         END IF;
         """
 
@@ -93,9 +122,9 @@ class Database:
         check_unique_seat_to_ride_trigger_query = """
         CREATE TRIGGER check_unique_seat_to_ride BEFORE INSERT ON ticket
         FOR EACH ROW
-        IF NEW.seat_no in (SELECT seat_no FROM ticket t WHERE NEW.ride_id = t.ride_id)
+            IF NEW.seat_no in (SELECT seat_no FROM ticket t WHERE NEW.ride_id = t.ride_id)
         THEN 
-        SIGNAL SQLSTATE '50002' SET MESSAGE_TEXT = 'Miejsce zajęte';
+            SIGNAL SQLSTATE '50002' SET MESSAGE_TEXT = 'Miejsce zajęte';
         END IF;
         """
 
@@ -110,26 +139,27 @@ class Database:
     def create_rides_table(self):
         create_ride_table_query = """
         CREATE TABLE IF NOT EXISTS rides (
-        ride_id VARCHAR(20) NOT NULL PRIMARY KEY,
-        arrival datetime NOT NULL,
-        departure datetime NOT NULL,
-        destination varchar(20) NOT NULL,
-        src varchar(20) NOT NULL,
-        seats int(11) NOT NULL,
-        price int(11) NOT NULL,
-        FOREIGN KEY (destination)
-            REFERENCES cities(city),
-        FOREIGN KEY (src)
-            REFERENCES cities(city)
+            ride_id VARCHAR(20) NOT NULL PRIMARY KEY,
+            arrival datetime NOT NULL,
+            departure datetime NOT NULL,
+            destination varchar(20) NOT NULL,
+            src varchar(20) NOT NULL,
+            seats int(11) NOT NULL,
+            price int(11) NOT NULL,
+            FOREIGN KEY (destination)
+                REFERENCES cities(city),
+            FOREIGN KEY (src)
+                REFERENCES cities(city)
         )"""
 
 
         insert_into_rides = """
         INSERT IGNORE INTO rides (ride_id, arrival, departure, destination, src, seats, price) VALUES
-        (1, '2022-02-10 10:03:00', '2022-02-10 09:01:00', 'Warszawa', 'Kraków', 23,  175),
-        (2, '2022-02-05 11:15:00', '2022-02-05 10:05:00', 'Tarnów', 'Wrocław', 21,  185),
-        (3, '2022-02-05 12:13:00', '2022-02-05 10:13:00', 'Rzeszów', 'Toruń', 23,  205),
-        (4, '2022-02-05 12:13:00', '2022-02-05 10:13:00', 'Będzin', 'Toruń', 23,  205)"""
+            (1, '2022-02-10 10:03:00', '2022-02-10 09:01:00', 'Warszawa', 'Kraków', 23,  175),
+            (5, '2022-02-10 10:11:00', '2022-02-10 09:21:00', 'Warszawa', 'Kraków', 24,  176),
+            (2, '2022-02-05 11:15:00', '2022-02-05 10:05:00', 'Tarnów', 'Wrocław', 21,  185),
+            (3, '2022-02-05 12:13:00', '2022-02-05 10:13:00', 'Rzeszów', 'Toruń', 23,  205),
+            (4, '2022-02-05 12:13:00', '2022-02-05 10:13:00', 'Będzin', 'Toruń', 23,  205)"""
 
         with self._connection.cursor() as cursor:
             cursor.execute(create_ride_table_query)
@@ -145,4 +175,8 @@ class Database:
 
 """if __name__ == "__main__":
     xd = Database()
-    xd.create_all()"""
+    xd.create_all()
+    lista = (xd.list_connections('Kraków', 'Warszawa'))
+    for i in lista:
+        print(i)
+"""
