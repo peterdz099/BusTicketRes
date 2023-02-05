fc2819ab45fba07bb5adf03a0aa13a6cfad07413
ben <bobusbogdan@gmail.com>
2023-02-05 16:36:47 +0100

    pobieranie listy polaczen bez dat jeszcze

--- database_handler/init_db.py
+++ database_handler/init_db.py
@@ -26,9 +26,26 @@ class Database:
     def close_connection(self):
         self._connection.close()
 
+    def get_free_seats(self, ride_id):
+        get_max_seats_query = """
+                               SELECT seats FROM rides WHERE
+                               ride_id = %s
+               """
+        get_taken_seats_query = """
+                                SELECT count(*) FROM ticket WHERE
+                                ride_id = %s"""
+        max_seats = 0
+        taken_seats = 0
+        with self._connection.cursor() as cursor:
+            cursor.execute(get_taken_seats_query, (ride_id,))
+            taken_seats = cursor.fetchall()
+            cursor.execute(get_max_seats_query, (ride_id,))
+            max_seats = cursor.fetchall()
+
+        return max_seats[0][0]-taken_seats[0][0]
     def list_cities(self):
         list_city_query = """
-        SELECT * FROM cities
+        SELECT city FROM cities
         """
         city_list = []
         temp = []
@@ -41,16 +58,18 @@ class Database:
                 city_list.append(field[0])
         return city_list
 
-    def list_connections(self, source, destination):
+    def list_connections(self, source, destination, date):
         list_connections_query = """
         SELECT * FROM rides WHERE 
         src = %s
         AND destination = %s
+        AND date(departure) = %s
+        AND departure < NOW()
         """
 
         connections_list = []
 
-        connection_values = (source, destination)
+        connection_values = (source, destination, date)
         temp = None
         with self._connection.cursor() as cursor:
             cursor.execute(list_connections_query, connection_values)
@@ -93,15 +112,16 @@ class Database:
     def create_cities_table(self):
         create_cities_table_query = """
         CREATE TABLE IF NOT EXISTS cities (
-            city varchar(255) NOT NULL UNIQUE)"""
+            city varchar(255) NOT NULL UNIQUE,
+            img_link VARCHAR(255))"""
 
         insert_into_cities_query = """
-        INSERT IGNORE INTO cities (city) VALUES
-            ('Toruń'),
-            ('Kraków'),
-            ('Warszawa'),
-            ('Bydgoszcz'),
-            ('Rzeszów')
+        INSERT IGNORE INTO cities (city, img_link) VALUES
+            ('Toruń', 'https://imgs.search.brave.com/Soh5XOt330xU5SPGKU7Ffq25Ts1mHJXtiHxpQLrZL90/rs:fit:1200:1060:1/g:ce/aHR0cDovL3d3dy53/ZWlzcy10cmF2ZWwu/Y29tL3dwLWNvbnRl/bnQvdXBsb2Fkcy8y/MDE4LzAxL1RvcnVu/LXN3aWV0by5qcGc'),
+            ('Kraków', 'https://imgs.search.brave.com/HMdj6Gmuu4WVD2xkPDeiTEly-cM83jR8-6q-yeNO1Ak/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly9kbWNw/b2xhbmQuY29tL3dw/LWNvbnRlbnQvdXBs/b2Fkcy8yMDE3LzAy/L2tyYWtvdzMuanBn'),
+            ('Warszawa', 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/Poland-00808_-_Castle_Square_%2831215382745%29.jpg/800px-Poland-00808_-_Castle_Square_%2831215382745%29.jpg'),
+            ('Bydgoszcz', 'https://imgs.search.brave.com/eM6ucCwizZvIhNnDJ__ZD7J3x1r_GjqkpZoasSOh_5A/rs:fit:1200:960:1/g:ce/aHR0cHM6Ly93d3cu/c3RheXBvbGFuZC5j/b20vd3AtY29udGVu/dC91cGxvYWRzLzIw/MTcvMDgvYnlkZ29z/emN6LTkwNDA5M18x/MjgwLmpwZw'),
+            ('Rzeszów', 'https://imgs.search.brave.com/MHbxCQ_6Llfcgso4cI9Hk8aOsukadb8HtLoMFmeSk1k/rs:fit:1110:600:1/g:ce/aHR0cHM6Ly9zdGF0/aWMucG9sc2tpZXN6/bGFraS5wbC96ZGpl/Y2lhL3d5Y2llY3pr/aS8yMDE2LTA0LzEx/MTBfNjAwLzMyNjI5/LW56LTE0NjEyMzcz/MzIuanBn')
         """
 
         with self._connection.cursor() as cursor:
@@ -112,8 +132,8 @@ class Database:
     def create_ticket_table(self):
         create_ticket_table_query = """
         CREATE TABLE IF NOT EXISTS ticket (
-            ticket_id VARCHAR(20) NOT NULL PRIMARY KEY,
-            ride_id VARCHAR(20) NOT NULL,
+            ticket_id VARCHAR(53) NOT NULL PRIMARY KEY,
+            ride_id VARCHAR(50) NOT NULL,
             user_id int(11) NOT NULL,
             seat_no int(11) NOT NULL,
             cost int(11) NOT NULL,
@@ -159,7 +179,7 @@ class Database:
     def create_rides_table(self):
         create_ride_table_query = """
         CREATE TABLE IF NOT EXISTS rides (
-            ride_id VARCHAR(20) NOT NULL PRIMARY KEY,
+            ride_id VARCHAR(50) NOT NULL PRIMARY KEY,
             arrival datetime NOT NULL,
             departure datetime NOT NULL,
             destination varchar(20) NOT NULL,
@@ -175,11 +195,10 @@ class Database:
 
         insert_into_rides = """
         INSERT IGNORE INTO rides (ride_id, arrival, departure, destination, src, seats, price) VALUES
-            (1, '2022-02-10 10:03:00', '2022-02-10 09:01:00', 'Warszawa', 'Kraków', 23,  175),
-            (5, '2022-02-10 10:11:00', '2022-02-10 09:21:00', 'Warszawa', 'Kraków', 24,  176),
-            (2, '2022-02-05 11:15:00', '2022-02-05 10:05:00', 'Tarnów', 'Wrocław', 21,  185),
-            (3, '2022-02-05 12:13:00', '2022-02-05 10:13:00', 'Rzeszów', 'Toruń', 23,  205),
-            (4, '2022-02-05 12:13:00', '2022-02-05 10:13:00', 'Będzin', 'Toruń', 23,  205)"""
+            ('1-WaKrk-202302101003', '2023-02-10 10:03:00', '2023-02-10 09:01:00', 'Warszawa', 'Kraków', 23,  175),
+            ('5-WaKrk-202302101011', '2023-02-10 10:11:00', '2023-02-10 09:21:00', 'Warszawa', 'Kraków', 24,  176),
+            ('3-RzeTor-202302071213', '2023-02-07 12:13:00', '2023-02-07 10:13:00', 'Rzeszów', 'Toruń', 23,  205)
+            """
 
         with self._connection.cursor() as cursor:
             cursor.execute(create_ride_table_query)
@@ -196,5 +215,5 @@ class Database:
 if __name__ == "__main__":
     xd = Database()
     xd.create_all()
-    print(xd.list_cities())
+    print(xd.get_free_seats(1))
 
